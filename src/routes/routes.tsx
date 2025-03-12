0b9c33b82f13b0309d7e1fcc370d40c63557b1c0
Azimov Shohruh <100364814+Shohruh003@users.noreply.github.com>
2025-03-13 03:34:48 +0500

    second commit

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { FC } from 'react';
-import NotFound from '../components/views/NotFound';
-import Profile from '../components/views/Profile';
-import Home from '../components/views/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { FC } from "react";
+import NotFound from "../components/views/NotFoundComponent/NotFound";
+import Home from "../components/views/MainComponent/Home/Home";
+import Characteristic from "../components/views/MainComponent/Characteristic/Characteristic";
 
 const RoutesComponent: FC = () => {
-    return (
-        <Router 
-            future={{
-                v7_relativeSplatPath: true,
-                v7_startTransition: true,
-            }}
-        >
-            <Routes>
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/" element={<Home />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router
+      future={{
+        v7_relativeSplatPath: true,
+        v7_startTransition: true,
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Characteristic />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default RoutesComponent;
--- src/services/mock/Products.ts
+++ src/services/mock/Products.ts
@@ -1,33 +1,69 @@
-interface IProduct {
-    id: number;
-    name: string;
-    price: number;
-    description: string;
-    image?: string;
-}
+import { IProduct } from "../../types/types";
 
 const products: IProduct[] = [
-    {
-        id: 1,
-        name: 'Product 1',
-        price: 100,
-        description: 'Description for product 1',
-        image: 'https://www.foodstoreint.co.uk/images/brands2.png',
-    },
-    {
-        id: 2,
-        name: 'Product 2',
-        price: 200,
-        description: 'Description for product 2',
-        image: 'https://www.foodstoreint.co.uk/images/brands2.png',
-    },
-    {
-        id: 3,
-        name: 'Product 3',
-        price: 300,
-        description: 'Description for product 3',
-        image: 'https://www.foodstoreint.co.uk/images/brands2.png',
-    },
+  {
+    id: 1,
+    price: 100,
+    name: "Knife Sharpener Tool Professional",
+    image: "https://www.foodstoreint.co.uk/images/brands2.png",
+    stock: "yes",
+    store: "Media Park",
+    location: "Tashkent",
+    code: 4325,
+    characteristics: [
+      { label: "Код товара", value: "MGN93RU/A" },
+      { label: "Производитель", value: "Apple" },
+      { label: "Разрешение экрана", value: "2560x1600" },
+      { label: "Количество ядер процессора", value: "8" },
+      { label: "Оптический привод", value: "Отсутствует" },
+      { label: "Объем памяти", value: "16 ГБ" },
+      { label: "Тип экрана", value: "IPS" },
+      { label: "Операционная система", value: "macOS" },
+    ],
+    description: "Оптический привод С новой силой. С появлением чипа Apple M1 наш самый тонкий и лёгкий ноутбук полностью преобразился. Центральный процессор теперь работает до 3,5 раза быстрее. Графический — до 5 раз. А благодаря нашей передовой системе Neural Engine скорость машинного обучения возросла до 9 раз. Новый Mac. Book Air работает без подзарядки дольше, чем предыдущие модели."
+  },
+  {
+    id: 2,
+    price: 200,
+    name: "Knife Sharpener Tool Professional",
+    image: "https://www.foodstoreint.co.uk/images/brands2.png",
+    stock: "no",
+    store: "Media Park",
+    location: "Tashkent",
+    code: 2345,
+    characteristics: [
+        { label: "Код товара", value: "MGN93RU/A" },
+        { label: "Производитель", value: "Apple" },
+        { label: "Разрешение экрана", value: "2560x1600" },
+        { label: "Количество ядер процессора", value: "8" },
+        { label: "Оптический привод", value: "Отсутствует" },
+        { label: "Объем памяти", value: "16 ГБ" },
+        { label: "Тип экрана", value: "IPS" },
+        { label: "Операционная система", value: "macOS" },
+      ],
+      description: "Оптический привод С новой силой. С появлением чипа Apple M1 наш самый тонкий и лёгкий ноутбук полностью преобразился. Центральный процессор теперь работает до 3,5 раза быстрее. Графический — до 5 раз. А благодаря нашей передовой системе Neural Engine скорость машинного обучения возросла до 9 раз. Новый Mac. Book Air работает без подзарядки дольше, чем предыдущие модели."
+  },
+  {
+    id: 3,
+    price: 300,
+    name: "Knife Sharpener Tool Professional",
+    image: "https://www.foodstoreint.co.uk/images/brands2.png",
+    stock: "yes",
+    store: "Media Park",
+    location: "Tashkent",
+    code: 123,
+    characteristics: [
+        { label: "Код товара", value: "MGN93RU/A" },
+        { label: "Производитель", value: "Apple" },
+        { label: "Разрешение экрана", value: "2560x1600" },
+        { label: "Количество ядер процессора", value: "8" },
+        { label: "Оптический привод", value: "Отсутствует" },
+        { label: "Объем памяти", value: "16 ГБ" },
+        { label: "Тип экрана", value: "IPS" },
+        { label: "Операционная система", value: "macOS" },
+      ],
+      description: "Оптический привод С новой силой. С появлением чипа Apple M1 наш самый тонкий и лёгкий ноутбук полностью преобразился. Центральный процессор теперь работает до 3,5 раза быстрее. Графический — до 5 раз. А благодаря нашей передовой системе Neural Engine скорость машинного обучения возросла до 9 раз. Новый Mac. Book Air работает без подзарядки дольше, чем предыдущие модели."
+  },
 ];
 
 export default products;
